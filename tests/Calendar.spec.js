9d00989d1e1be0fc8bbe38262e7afd28419aedbf
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2026-06-09 15:49:39 +0300

    Verifying that the correct date is selected

--- tests/Calendar.spec.js
+++ tests/Calendar.spec.js
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test("Calendar Validations", async ({ page }) => {
-    const month = "6";
-    const day = "15";
-    const year = "2026";
+    const month = "8";
+    const day = "12";
+    const year = "2027";
+    const expectedList = [month, day, year];
 
     await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
     await page.locator(".react-date-picker__inputGroup").click();
@@ -12,4 +13,10 @@ test("Calendar Validations", async ({ page }) => {
     await page.getByText(year).click();
     await page.locator(".react-calendar__tile").nth(Number(month) - 1).click();
     await page.locator(`//abbr[text()='${day}']`).click();
+
+    const inputs = page.locator(".react-date-picker__inputGroup input:not([hidden])");
+
+    for (let i = 0; i < expectedList.length; i++) {
+        await expect(inputs.nth(i)).toHaveValue(expectedList[i]);
+    }
 });
