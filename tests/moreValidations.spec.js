fcb9daf3248d9b78a60f21ceaaf4471970079a23
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2026-06-10 17:21:13 +0300

    Adding Popup validatoins and handling

--- tests/moreValidations.spec.js
+++ tests/moreValidations.spec.js
@@ -10,3 +10,15 @@ test("Validating visibility of elements", async ({ page }) => {
     await page.locator("#hide-textbox").click();
     await expect(testInput).toBeHidden();
 });
+
+test.only("Popup validations", async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice");
+
+
+    page.on("dialog", async dialog => {
+        // console.log(dialog.message());
+        await dialog.accept();
+    });
+
+    await page.locator("#confirmbtn").click();
+});
