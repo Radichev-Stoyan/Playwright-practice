7d8858308de257aef609ea00f03faa34a8d386b7
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2026-06-10 16:52:50 +0300

    Edited the name of the 1st test

--- tests/moreValidations.spec.js
+++ tests/moreValidations.spec.js
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test("Popup validations", async ({ page }) => {
+test("Validating visibility of elements", async ({ page }) => {
     const testInput = page.locator("#displayed-text");
 
     await page.goto("https://rahulshettyacademy.com/AutomationPractice");
+    // Checking if the input field is visible
     await expect(testInput).toBeVisible();
+    // Hiding the input field and validating if it's hidden
     await page.locator("#hide-textbox").click();
     await expect(testInput).toBeHidden();
 });
