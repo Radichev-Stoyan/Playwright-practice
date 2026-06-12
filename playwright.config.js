a51a6e36e0893784322145acca8186f73607608a
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2026-06-12 15:02:36 +0300

    Edited the playright config file

--- playwright.config.js
+++ playwright.config.js
@@ -1,23 +1,20 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
-const config = ({
+export default defineConfig({
   testDir: './tests',
   timeout: 40 * 1000,
+
   expect: {
     timeout: 5000,
   },
+
   reporter: 'html',
+
   use: {
     browserName: 'chromium',
-    // headless: false,
     headless: process.env.CI ? true : false,
     screenshot: 'on',
     trace: 'retain-on-failure',
-  }
+  },
 });
-
-module.exports = config;
